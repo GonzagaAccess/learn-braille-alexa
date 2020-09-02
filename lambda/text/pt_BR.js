114ae9df65eb1701b2cdb7bc5e5a47217ffb0336
RafaelGSS <rafael.nunu@hotmail.com>
2020-09-01 22:20:10 -0300

    update(text): adjust history text

--- lambda/text/pt_BR.js
+++ lambda/text/pt_BR.js
@@ -18,9 +18,14 @@ module.exports = {
     desenvolvimento
   `,
   history: `
-    O braille tem sua origem com um rapaz chamado Louis Braille que ficou completamente cego após acidentalmente ferir os olhos com uma ferramenta do pai aos 3 anos de idade.
-    Após alguns anos ele conheceu um homem chamado Charles Barbier que havia desenvolvido uma ‘’escrita noturna’’ para que os soldados pudessem ler mesmo em total escuridão e sem ruídos, porém era muito complexo e foi abandonado. Esse projeto foi então aprimorado por Louise Braille aos 16 anos.
-    Hoje, o braile é de fácil leitura. Ela se inicia da esquerda para direita, possui sessenta e quatro símbolos em relevo, sendo combinações de pontos em duas colunas de três pontos cada uma.
-    Ele é utilizado por pessoas com baixa visão ou totalmente cegos. O braille chegou no Brasil onze anos após sua criação pelas mãos de Álvares de Azevedo com a criação do Instituto Menos Cegos que hoje é chamado de Benjamin Constant.
+  <speak>
+    O braille tem sua origem com um rapaz chamado Louis Braille que <amazon:emotion name="disappointed" intensity="medium"> ficou completamente cego após acidentalmente ferir os olhos com uma ferramenta do pai aos 3 anos de idade. </amazon:emotion>
+    Após alguns anos ele conheceu um homem chamado Charles Barbier que havia desenvolvido uma "escrita noturna" para que os soldados pudessem ler mesmo em total escuridão e sem ruídos, porém era muito complexo e foi abandonado.
+    Esse projeto foi então aprimorado por Louis Braille aos 16 anos.
+    Hoje, o braille é de fácil leitura. Ela se inicia da esquerda para direita, possui sessenta e quatro símbolos em relevo,
+    sendo combinações de pontos em duas colunas de três pontos cada uma.
+    Ele é utilizado por pessoas com baixa visão ou totalmente cegos.
+    O braille chegou no Brasil onze anos após sua criação pelas mãos de Álvares de Azevedo com a criação do Instituto Menos Cegos, que hoje é chamado de Benjamin Constant.
+  </speak>
   `
 }
