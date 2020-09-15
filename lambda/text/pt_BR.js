0dd4f3a804e7be2769d26fede6382f8fd2ded847
RafaelGSS <rafael.nunu@hotmail.com>
2020-09-15 20:38:57 -0300

    update(text): adjust history of braille

--- lambda/text/pt_BR.js
+++ lambda/text/pt_BR.js
@@ -19,13 +19,29 @@ module.exports = {
   `,
   history: `
   <speak>
-    O braille tem sua origem com um rapaz chamado Louis Braille que <amazon:emotion name="disappointed" intensity="medium"> ficou completamente cego após acidentalmente ferir os olhos com uma ferramenta do pai aos 3 anos de idade. </amazon:emotion>
-    Após alguns anos ele conheceu um homem chamado Charles Barbier que havia desenvolvido uma "escrita noturna" para que os soldados pudessem ler mesmo em total escuridão e sem ruídos, porém era muito complexo e foi abandonado.
-    Esse projeto foi então aprimorado por Louis Braille aos 16 anos.
-    Hoje, o braille é de fácil leitura. Ela se inicia da esquerda para direita, possui sessenta e quatro símbolos em relevo,
-    sendo combinações de pontos em duas colunas de três pontos cada uma.
-    Ele é utilizado por pessoas com baixa visão ou totalmente cegos.
-    O braille chegou no Brasil onze anos após sua criação pelas mãos de Álvares de Azevedo com a criação do Instituto Menos Cegos, que hoje é chamado de Benjamin Constant.
+    APRESENTAÇÃO
+
+    Louis Braille nasceu em janeiro de 1809, em Coupvray, França. Aos seus 3 anos de idade, ao brincar com as ferramentas da oficina de seu pai, Louis perfurou seu olho esquerdo e, por uma infecção não tratada, perdeu a visão do olho direito aos cinco anos. 
+    As dificuldades enfrentadas por Louis Braille em seus estudos o levaram a se preocupar com a possibilidade da criação de um sistema de escrita para cegos. Em 1824 Braille tem acesso à “Escrita Noturna” do Capitão Charles Barbier de la Serre. Barbier criou uma escrita em relevo para que os militares pudessem receber ordens de batalha e lê-las mesmo no escuro.
+    O contato com a escrita de Barbier deu base para que Louis Braille, aos 15 anos, criasse um alfabeto em relevo, de leitura tátil, usado até hoje, pelos cegos do mundo todo. Tal invenção recebeu seu nome: Alfabeto Braille.
+    As possíveis 64 combinações que formam esse alfabeto, além das letras, originaram a pontuação, a acentuação, os sinais matemáticos e a notação musical.
+
+    EDUCAÇÃO DO CEGO:
+
+    O cego, indivíduo privado do sentido da visão desde o nascimento ou por doença ou acidente, era, na Antiguidade, criatura marginalizada, sendo o seu destino a mendicância.
+
+    Em 1784, Valentin Hauy, considerado o “pai da educação dos cegos”, fundou, em Paris, a primeira escola para cegos: Instituto Nacional para Meninos Cegos. Foi criador de uma escrita em relevo, para permitir a leitura aos cegos, pelo tato, bem como de um ensaio para a educação dos cegos.
+
+    Louis Braille, um dos discípulos de Valentin Hauy, entrou para o Instituto de Cegos de Paris com sete anos, quatro após ter ficado cego. Foi músico distinto e depois professor no mesmo Instituto. Entregou-se ao estudo de uma escrita para uso dos cegos, hoje universalmente conhecida por Sistema Braille, cujas peculiaridades são descritas em seu “processo de escrever para uso dos cegos”.
+
+    Várias foram as tentativas para conseguir um meio de facilitar aos cegos a leitura e a escrita. A primeira a interessar a opinião geral foi a de Hauy, consistente num sistema de letra comum em relevo. Método de difícil aplicação e de má compreensão por parte dos cegos sofreu várias modificações para as quais contribuíram diferentes autores, ora alternando maiúsculas com minúsculas, ora empregando pontos dispostos de maneira a facilitar as letras mais difíceis.
+
+    As tentativas continuaram, até que Louis, aliando ao devotamento as suas qualidades de excelente professor, e conhecendo as dificuldades que o sistema de ensaio apresentava para seus alunos, criou, com seis pontos salientes, 63 combinações, que representavam letras simples e acentuadas, pontuações, algarismos, sinais algébricos, contrações estenográficas e sinais para música. Em colaboração com Foucault, Braille introduziu novas modificações no processo, para mais fácil comunicação entre indivíduos videntes com os cegos. O método foi, mais tarde, modificado por Victor Ballu, antigo aluno de Braille. Várias foram as tentativas para conseguir um meio de facilitar aos cegos a leitura e a escrita. A primeira a interessar a opinião geral foi a de Hauy, consistente num sistema de letra comum em relevo. Método de difícil aplicação e de má compreensão por parte dos cegos sofreu várias modificações para as quais contribuíram diferentes autores, ora alternando maiúsculas com minúsculas, ora empregando pontos dispostos de maneira a facilitar as letras mais difíceis.
+
+    Com o impulso da escrita Braille, surgiram numerosas escolas para cegos e bibliotecas especializadas. No Brasil, o Instituto Benjamin Constant, fundado em 1854, a Fundação para o Livro do Cego (hoje Fundação Dorina Nowill para Cegos), o Instituto Padre Chico, o Instituto São Rafael, o Instituto Santa Luzia, o Instituto de Cegos da Bahia e a Companhia Nacional de Educação dos Cegos são as principais entidades que se dedicam à assistência e educação dos cegos. 
+
+    Atualmente, os cegos estão integrados ao trabalho, sendo úteis à sociedade e não se constituindo em um encargo para a mesma. Podem ser revisores de peças por intermédio do tato, dedicar-se à vida artística como cantores e etc.
+    Em alguns casos, a limitação visual pode até funcionar como uma vantagem em relação ao indivíduo vidente, citando-se como exemplo as atividades nas quais o tato tem papel preponderante, pois os cegos têm esse sentido altamente desenvolvido.
   </speak>
   `
 }
