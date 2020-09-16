3e02316ef2c1c9ee0124ab855d0013b869038a8c
RafaelGSS <rafael.nunu@hotmail.com>
2020-09-15 22:06:46 -0300

    add try catch on launchIntentHandler

--- lambda/handlers/launchIntentHandler.js
+++ lambda/handlers/launchIntentHandler.js
@@ -1,18 +1,23 @@
 const { getMessage } = require('../text/text');
 
 const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const availableTasks = getMessage('availableTasks');
-        const speakOutput = `${getMessage('welcome')}\n${availableTasks}`;
+  canHandle(handlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    try {
 
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(availableTasks)
-            .getResponse();
+      const availableTasks = getMessage('availableTasks');
+      const speakOutput = `${getMessage('welcome')}\n${availableTasks}`;
+
+      return handlerInput.responseBuilder
+        .speak(speakOutput)
+        .reprompt(availableTasks)
+        .getResponse();
+    } catch (e) {
+      console.log('Error on ', e)
     }
+  }
 };
 
 module.exports = { LaunchRequestHandler }
